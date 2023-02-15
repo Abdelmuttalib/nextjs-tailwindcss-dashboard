8da0cc262313eb89b92378cf444812c25dc2fbf0
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2023-02-15 14:34:53 +0800

    feat(theme): resolve gradient element prop names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { ArrowRightIcon, MoonIcon, SunIcon } from '@heroicons/react/20/solid';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTheme } from 'next-themes';
 
 import { dashboardLinks } from '@/components/layout/SideBar';
+import { IconButton, IconLink } from '@/components/ui/icon-button';
 
 export default function HomePage() {
   const { t } = useTranslation();
+  const { pathname, locale } = useRouter();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <div className='isolate'>
-      <div className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-10rem]'>
+    <div className='isolate dark:bg-gray-900'>
+      <div className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl dark:hidden sm:top-[-10rem]'>
         <svg
           className='relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]'
           viewBox='0 0 1155 678'
@@ -43,19 +48,40 @@ export default function HomePage() {
           <div className='mx-auto max-w-5xl py-32 sm:py-48 lg:py-56'>
             <div className='text-center'>
               <h1 className='h2 sm:display-sm lg:display-lg bg-gradient-to-br from-primary-400 to-primary-800 bg-clip-text text-transparent'>
-                InView
+                {t('app.name')}
               </h1>
-              <p className='h2 bg-gradient-to-br from-gray-800 to-primary-800 bg-clip-text text-transparent'>
-                {' '}
-                Maintenance Dashboard
+              <p className='h2 bg-gradient-to-br from-gray-800 to-primary-800 bg-clip-text text-transparent dark:from-primary-300 dark:to-gray-300'>
+                {t('app.description')}
               </p>
+
+              <div className='mt-3 flex justify-center gap-3'>
+                <IconLink
+                  href={pathname}
+                  locale={locale === 'en' ? 'zh' : 'en'}
+                  variant='outline'
+                  size='lg'
+                >
+                  {locale}
+                </IconLink>
+                <IconButton
+                  type='button'
+                  variant='outline'
+                  onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+                >
+                  {theme === 'light' ? (
+                    <SunIcon className='w-7' />
+                  ) : (
+                    <MoonIcon className='w-7 p-0.5' />
+                  )}
+                </IconButton>
+              </div>
 
               <div className='3xl:grid-cols-3 my-8 grid grid-cols-1 gap-3 md:grid-cols-2'>
                 {dashboardLinks.map((link) => (
                   <Link
                     key={link.href}
                     href={link.href}
-                    className='group inline-flex items-center justify-between rounded-lg border-2  bg-gradient-to-br from-primary-50 to-primary-100 px-5 py-6 text-gray-700 hover:bg-primary-100 hover:to-primary-200'
+                    className='group inline-flex items-center justify-between rounded-lg border-2 bg-gradient-to-br from-primary-50 to-primary-100 px-5 py-6 text-gray-700 hover:bg-primary-100 hover:to-primary-200 dark:border-gray-800/50 dark:from-gray-900 dark:to-gray-900 dark:text-primary-200 hover:dark:text-primary-400'
                     target='_blank'
                     rel='noopener noreferrer'
                   >
@@ -68,7 +94,7 @@ export default function HomePage() {
               </div>
             </div>
           </div>
-          <div className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]'>
+          <div className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl dark:hidden sm:top-[calc(100%-40rem)]'>
             <svg
               className='relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[32.375rem]'
               viewBox='0 0 1155 678'
