3030684958b9058418ad895886cdfc9efab08cab
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2023-03-07 09:37:59 +0800

    feat(toasts): update toasts and use sooner for toast management

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -3,39 +3,25 @@ import {
   DevicePhoneMobileIcon,
   XMarkIcon,
 } from '@heroicons/react/20/solid';
-import { useEffect } from 'react';
 
 import cn from '@/lib/cn';
-import useToast, { ToastT } from '@/hooks/useToast';
 
-type Props = {
-  id: number;
-  description: ToastT['description'];
-  type: ToastT['type'];
+export type ToastT = {
+  type: 'error' | 'sync' | 'device' | 'Device';
+  description: string;
 };
 
-const Toast = ({ id, description, type }: Props) => {
-  const { removeToast } = useToast();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      removeToast(id);
-    }, 5000);
-    return () => {
-      clearTimeout(timer);
-    };
-  });
-
+const Toast = ({ description, type }: ToastT) => {
   const typeToLowerCase = type.toLowerCase();
 
   return (
     <div
       className={cn(
-        'relative mx-auto mt-2 flex h-20 w-11/12 animate-fade-in-up items-center justify-between truncate rounded-lg border-2 bg-gradient-to-br from-white  py-4 pl-4 pr-2 text-sm shadow-xl dark:border-gray-700 dark:from-gray-800',
+        'inline-flex h-20 w-full min-w-[22.5rem] items-center justify-between truncate rounded-lg border-2 bg-gradient-to-br from-white py-4 pl-4 pr-2 text-sm shadow-xl dark:border-gray-700/70 dark:from-gray-900/90 sm:w-96 sm:min-w-[22.5rem]',
         {
           'to-red-50 shadow-red/10 dark:to-gray-900':
             typeToLowerCase === 'error',
-          'to-blue-50 shadow-blue-500/10 dark:to-gray-900':
+          'to-blue-100 shadow-blue-500/10 dark:to-gray-900':
             typeToLowerCase === 'sync',
           'to-teal-50 shadow-teal-500/10 dark:to-gray-900':
             typeToLowerCase === 'device',
@@ -54,18 +40,20 @@ const Toast = ({ id, description, type }: Props) => {
         </div>
       )}
       {typeToLowerCase === 'device' && (
-        <div className='block w-fit rounded-full bg-teal-600/20 p-2'>
+        <div className='block w-fit rounded-full bg-teal-500/20 p-2'>
           <DevicePhoneMobileIcon className='h-6 text-teal-500' />
         </div>
       )}
-      <p className='body-sm sm:body-md mx-2 w-full truncate'>{description}</p>
-      <button
+      <p className='body-md mx-2 w-full truncate text-gray-900 dark:text-gray-200'>
+        {description}
+      </p>
+      {/* <button
         className='rounded-full p-1.5 text-gray-700 outline-transparent hover:bg-gray-100 focus:outline-transparent dark:text-gray-500 dark:hover:bg-gray-700'
         onClick={() => removeToast(id)}
         aria-label='Close'
       >
         <XMarkIcon className='w-6' />
-      </button>
+      </button> */}
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,71 @@
 import { AppProps } from 'next/app';
 import { appWithTranslation, I18n, i18n } from 'next-i18next';
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useRef } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { io } from 'socket.io-client';
+import { toast, Toaster } from 'sonner';
+import { useSWRConfig } from 'swr';
 
 import '@/styles/globals.css';
 
-import ToastProvider from '@/components/Toast/ToastProvider';
+import { fetchAPI } from '@/lib/api';
+
+import Toast from '@/components/Toast/Toast';
+
+const SOCKET_IO_URL = process.env.NEXT_PUBLIC_SOCKET_IO_URL as string;
+const socket = io(SOCKET_IO_URL);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { mutate } = useSWRConfig();
+  const socketInitialized = useRef(false);
+
+  function initSocketIO() {
+    socket.on(
+      'Notification',
+      (message: {
+        notificationType: 'error' | 'sync' | 'device' | 'Device';
+        description: string;
+      }) => {
+        mutate('/notifications');
+        toast.custom(() => (
+          <Toast
+            description={message.description}
+            type={message.notificationType}
+          />
+        ));
+
+        fetchAPI.get('/check-devices-algorithm');
+      }
+    );
+  }
+
+  // function disconnectSocketIO() {
+  //   socket.disconnect();
+  // }
+
+  useEffect(() => {
+    if (socketInitialized.current) return;
+    socketInitialized.current = true;
+    initSocketIO();
+
+    // return disconnectSocketIO();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <ThemeProvider attribute='class'>
-      <ToastProvider>
-        <I18nextProvider i18n={i18n as I18n}>
-          <Component {...pageProps} />
-        </I18nextProvider>
-      </ToastProvider>
+      <Toaster
+        position='top-right'
+        expand
+        visibleToasts={6}
+        closeButton
+        style={{
+          marginRight: '1.5rem',
+        }}
+      />
+      <I18nextProvider i18n={i18n as I18n}>
+        <Component {...pageProps} />
+      </I18nextProvider>
     </ThemeProvider>
   );
 }
